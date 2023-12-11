54b93dac544feb1e20558ab0f365dc4134bcda08
Abinesh <63021688+abineshranganathan@users.noreply.github.com>
2023-12-11 20:49:30 +0000

    listed salse and services

---
+++ assets/data/salesAndServices.js
@@ -0,0 +1,49 @@
+export const salesAndServiceData =
+{
+    "services" : [
+        {
+            "service": "Washing Machine",
+            "image": "./assets/images/services/laundry-machine.png",
+            "serviceDesc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sapiente aliquam.",
+            "action": ""
+        },
+        {
+            "service": "Air Conditioner",
+            "image": "./assets/images/services/air-conditioner.png",
+            "serviceDesc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sapiente aliquam.",
+            "action": ""
+        },
+        {
+            "service": "Refrigerator",
+            "image": "./assets/images/services/refrigerator.png",
+            "serviceDesc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sapiente aliquam.",
+            "action": ""
+        },
+        {
+            "service": "Water Purifier",
+            "image": "./assets/images/services/water-purifier.png",
+            "serviceDesc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sapiente aliquam.",
+            "action": ""
+        },
+        {
+            "service": "Inverter Battery",
+            "image": "./assets/images/services/inverter.png",
+            "serviceDesc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sapiente aliquam.",
+            "action": ""
+        }
+    ],
+    "sales": [
+        {
+            "product": "Water Purifier",
+            "image": "./assets/images/sales/water-filter.png",
+            "productDesc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sapiente aliquam.",
+            "action": ""
+        },
+        {
+            "product": "Inverter Battery",
+            "image": "./assets/images/sales/accumulator.png",
+            "productDesc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sapiente aliquam.",
+            "action": ""
+        }
+    ]
+}
--- script/main.js
+++ script/main.js
@@ -1,6 +1,9 @@
 const actionMenuToggle = document.getElementById("menuToggle");
 const menuList = document.getElementById("menuList");
 const header = document.getElementById("header");
+const servicesCardList = document.getElementById("service-card-list");
+const salesCardList = document.getElementById("sales-card-list");
+import { salesAndServiceData } from "../assets/data/salesAndServices.js";
 
 //methods
 const menuToggle = () => {
@@ -25,7 +28,47 @@ const handleScroll = () => {
     }
   }
 };
+salesAndServiceData.services.forEach(data => {
 
+  const servicesCard = document.createElement('div');
+  servicesCard.innerHTML =
+  `
+  <div class="app-card">
+    <div class="app-card__img">
+      <img src=${data.image} width="100%" height="100%" alt=${data.service}>
+    </div>
+    <div>
+      <h5>${data.service}</h5>
+      <article class="app-card__desc">
+      ${data.serviceDesc}
+      </article>
+    </div>
+    <button class="app-card__btn app-card__btn-service">View in detail</button>
+  </div>
+  `
+  servicesCardList.appendChild(servicesCard);
+});
+
+salesAndServiceData.sales.forEach(data => {
+
+  const salesCard = document.createElement('div');
+  salesCard.innerHTML =
+  `
+  <div class="app-card">
+    <div class="app-card__img">
+      <img src=${data.image} width="100%" height="100%" alt=${data.product}>
+    </div>
+    <div>
+      <h5>${data.product}</h5>
+      <article class="app-card__desc">
+      ${data.productDesc}
+      </article>
+    </div>
+    <button class="app-card__btn app-card__btn-sales">View in detail</button>
+  </div>
+  `
+  salesCardList.appendChild(salesCard);
+});
 // ****user actions**** //
 // on scroll set header color background
 window.addEventListener("scroll", handleScroll);
