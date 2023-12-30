82e21eba3c52f2fbea02d117a19b068ec814ae9f
Abinesh <abineshranganathan@gmail.com>
2023-12-31 04:37:05 +0530

    completed services and sales page

--- assets/data/salesAndServices.js
+++ assets/data/salesAndServices.js
@@ -1,49 +1,83 @@
-export const salesAndServiceData =
-{
-    "services" : [
-        {
-            "service": "Washing Machine",
-            "image": "./assets/images/services/laundry-machine.png",
-            "serviceDesc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sapiente aliquam.",
-            "action": ""
-        },
-        {
-            "service": "Air Conditioner",
-            "image": "./assets/images/services/air-conditioner.png",
-            "serviceDesc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sapiente aliquam.",
-            "action": ""
-        },
-        {
-            "service": "Refrigerator",
-            "image": "./assets/images/services/refrigerator.png",
-            "serviceDesc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sapiente aliquam.",
-            "action": ""
-        },
-        {
-            "service": "Water Purifier",
-            "image": "./assets/images/services/water-purifier.png",
-            "serviceDesc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sapiente aliquam.",
-            "action": ""
-        },
-        {
-            "service": "Inverter Battery",
-            "image": "./assets/images/services/inverter.png",
-            "serviceDesc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sapiente aliquam.",
-            "action": ""
-        }
-    ],
-    "sales": [
-        {
-            "product": "Water Purifier",
-            "image": "./assets/images/sales/water-filter.png",
-            "productDesc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sapiente aliquam.",
-            "action": ""
-        },
-        {
-            "product": "Inverter Battery",
-            "image": "./assets/images/sales/accumulator.png",
-            "productDesc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sapiente aliquam.",
-            "action": ""
-        }
-    ]
-}
+export const salesAndServiceData = {
+  services: [
+    {
+      service: "Washing Machine",
+      image: "./assets/images/services/laundry-machine.png",
+      serviceDesc:
+        "Regular washing machine services ensure peak performance and longevity, keeping your laundry consistently clean and efficient.",
+      orgImg: "/assets/images/services//laundry-machine.png",
+      serviceLonDesc:
+        "Periodic washing machine maintenance is crucial for optimal functionality. Trained technicians conduct thorough inspections, addressing potential issues, lubricating moving parts, and fine-tuning the appliance. These measures not only prevent unexpected breakdowns but also contribute to an extended lifespan. By investing in routine services, you are not only ensuring consistently clean and efficient laundry results but also safeguarding your washing machine's overall health.",
+      navigatorId: "washing-machine",
+      action: "/pages/services/services.html#washing-machine",
+    },
+    {
+      service: "Air Conditioner",
+      image: "./assets/images/services/air-conditioner.png",
+      serviceDesc:
+        "Scheduled AC maintenance guarantees efficient cooling, lower energy consumption and a comfortable indoor environment for long-lasting performance.",
+      orgImg: "/assets/images/services/air-conditioner.png",
+      serviceLonDesc:
+        "Regular AC maintenance is a comprehensive approach to preserving your cooling system's efficiency. Skilled technicians meticulously clean and replace filters, inspect various components, and address potential issues. This not only reduces energy consumption but also ensures your AC's long-lasting and efficient performance in creating a comfortable indoor environment. Proactive maintenance enhances the overall resilience of your air conditioning unit.",
+      navigatorId: "air-conditioner",
+      action: "/pages/services/services.html#air-conditioner",
+    },
+    {
+      service: "Refrigerator",
+      image: "./assets/images/services/refrigerator.png",
+      serviceDesc:
+        "Routine fridge services preserve food freshness and extend the overall lifespan, ensuring reliable storage for perishables.",
+      orgImg: "/assets/images/services/refrigerator.png",
+      serviceLonDesc:
+        "Text: Consistent fridge services play a pivotal role in maintaining the freshness of your stored food items. During routine checks, technicians meticulously inspect and address potential issues, optimizing the refrigerator's performance. This comprehensive approach not only prevents unexpected breakdowns but also extends the overall lifespan of your fridge. By investing in regular maintenance, you're not just ensuring a reliable storage solution for perishables but also enhancing the efficiency of your refrigerator.",
+      navigatorId: "refrigerator",
+      action: "/pages/services/services.html#refrigerator",
+    },
+    {
+      service: "Water Purifier",
+      image: "./assets/images/services/water-purifier.png",
+      serviceDesc:
+        "One-liner: Regular RO system maintenance ensures clean and pure drinking water, safeguarding your health and the effectiveness of the purification system.",
+      orgImg: "/assets/images/services/water-purifier.png",
+      serviceLonDesc:
+        "The importance of regular RO system maintenance cannot be overstated. Skilled technicians conduct in-depth checks, replace filters, and address any potential issues to ensure the ongoing purity of your drinking water. This proactive approach safeguards both your health and the effectiveness of the water purification system. Investing in consistent maintenance is an investment in the reliability of your drinking water source, providing clean and pure water for you and your family.",
+      navigatorId: "water-purifier",
+      action: "/pages/services/services.html#water-purifier",
+    },
+    {
+      service: "Inverter Battery",
+      image: "./assets/images/services/inverter.png",
+      serviceDesc:
+        "Timely inverter battery services guarantee uninterrupted power and extend the battery's lifespan for continuous power to essential appliances.",
+      orgImg: "/assets/images/services/inverter.png",
+      serviceLonDesc:
+        "Text: Scheduled inverter battery services are a critical aspect of ensuring uninterrupted power supply during outages. Technicians conduct routine checks, promptly address potential issues, and optimize the battery's performance. This comprehensive approach not only guarantees uninterrupted power for your essential appliances but also contributes to the extended lifespan of the battery. Regular services enhance the reliability and longevity of your home appliances, providing continuous power and peace of mind during power outages.",
+      navigatorId: "inverter-battery",
+      action: "/pages/services/services.html#inverter-battery",
+    },
+  ],
+  sales: [
+    {
+      product: "Water Purifier",
+      image: "./assets/images/sales/water-filter.png",
+      productDesc:
+        "Elevate your water quality with our advanced RO systems, delivering clean and pure drinking water for the health and well-being of your family.",
+      orgImg: "/assets/images/sales/water-filter.png",
+      productLonDesc:
+        "Transform your water source into a haven of purity with our state-of-the-art RO systems. Crafted with precision and advanced technology, our systems are designed to remove impurities and contaminants, ensuring your family has access to the highest quality drinking water. Make the choice for health and well-being – choose our reliable and efficient water purification solutions that go beyond clean water; they provide peace of mind.",
+      navigatorId: "water-purifier",
+      action: "/pages/sales/sales.html#water-purifier",
+    },
+    {
+      product: "Inverter Battery",
+      image: "./assets/images/sales/accumulator.png",
+      productDesc:
+        "Power up your home with our durable inverter batteries, providing uninterrupted electricity to essential appliances for peace of mind during outages.",
+      orgImg: "/assets/images/sales/accumulator.png",
+      productLonDesc:
+        "When it comes to uninterrupted power supply, our inverter batteries stand out for their durability and reliability. Engineered for optimal performance, our batteries ensure continuous electricity to your essential appliances, offering not just power but also peace of mind during unexpected outages. Choose power you can depend on; choose our inverter batteries to keep your home running smoothly even when the lights go out.",
+      navigatorId: "inverter-battery",
+      action: "/pages/sales/sales.html#inverter-battery",
+    },
+  ],
+};
--- script/main.js
+++ script/main.js
@@ -4,9 +4,10 @@ const header = document.getElementById("header");
 const servicesCardList = document.getElementById("service-card-list");
 const salesCardList = document.getElementById("sales-card-list");
 const whyChooseUsList = document.getElementById("why-choose-us-list");
+const pageServices = document.getElementById("blog-services");
+const pageSales = document.getElementById("blog-sales");
 import { salesAndServiceData } from "../assets/data/salesAndServices.js";
 import { whyChooseUs } from "../assets/data/whyChooseUs.js";
-
 
 //methods
 const menuToggle = () => {
@@ -31,66 +32,138 @@ const handleScroll = () => {
     }
   }
 };
-salesAndServiceData.services.forEach(data => {
-
-  const servicesCard = document.createElement('div');
-  servicesCard.innerHTML =
-  `
-  <div class="app-card">
-    <div class="app-card__img">
-      <img src=${data.image} width="100%" height="100%" alt=${data.service}>
-    </div>
-    <div>
-      <h5>${data.service}</h5>
-      <article class="app-card__desc">
-      ${data.serviceDesc}
-      </article>
-    </div>
-    <button class="app-card__btn app-card__btn-service">View in detail</button>
-  </div>
-  `
-  servicesCardList.appendChild(servicesCard);
-});
-
-salesAndServiceData.sales.forEach(data => {
-
-  const salesCard = document.createElement('div');
-  salesCard.innerHTML =
-  `
-  <div class="app-card">
-    <div class="app-card__img">
-      <img src=${data.image} width="100%" height="100%" alt=${data.product}>
-    </div>
-    <div>
-      <h5>${data.product}</h5>
-      <article class="app-card__desc">
-      ${data.productDesc}
-      </article>
-    </div>
-    <button class="app-card__btn app-card__btn-sales">View in detail</button>
-  </div>
-  `
-  salesCardList.appendChild(salesCard);
-});
-
-whyChooseUs.lists.forEach(data => {
-  const whyChooseUsEle = document.createElement('div');
-  whyChooseUsEle.innerHTML = 
-  `
-    <div class="choose-us-item">
-      <div class="choose-us-item__icon">
-        <img src="${data.icon}" alt="${data.title}" width="100%" height="100%">
+if (servicesCardList) {
+  salesAndServiceData.services.forEach((data) => {
+    const servicesCard = document.createElement("div");
+    servicesCard.innerHTML = `
+    <div class="app-card">
+      <div class="app-card__img">
+        <img src=${data.image} width="100%" height="100%" alt=${data.service}>
       </div>
-      <div class="choose-us-item__content">
-        <h5 class="choose-us-item__heading">${data.title}</h5>
-        <article class="choose-us-item__desc">
-          ${data.content}
+      <div>
+        <h5>${data.service}</h5>
+        <article class="app-card__desc">
+        ${data.serviceDesc}
         </article>
       </div>
+      <a href=${data.action} class="app-card__btn app-card__btn-service">View in detail</a>
     </div>
-  `;
-  whyChooseUsList.appendChild(whyChooseUsEle);
-})
+    `;
+    servicesCardList.appendChild(servicesCard);
+  });
+}
+
+if (salesCardList) {
+  salesAndServiceData.sales.forEach((data) => {
+    const salesCard = document.createElement("div");
+    salesCard.innerHTML = `
+    <div class="app-card">
+      <div class="app-card__img">
+        <img src=${data.image} width="100%" height="100%" alt=${data.product}>
+      </div>
+      <div>
+        <h5>${data.product}</h5>
+        <article class="app-card__desc">
+        ${data.productDesc}
+        </article>
+      </div>
+      <a href=${data.action} class="app-card__btn app-card__btn-sales">View in detail</a>
+    </div>
+    `;
+    salesCardList.appendChild(salesCard);
+  });
+}
+if (whyChooseUsList) {
+  whyChooseUs.lists.forEach((data) => {
+    const whyChooseUsEle = document.createElement("div");
+    whyChooseUsEle.innerHTML = `
+      <div class="choose-us-item">
+        <div class="choose-us-item__icon">
+          <img src="${data.icon}" alt="${data.title}" width="100%" height="100%">
+        </div>
+        <div class="choose-us-item__content">
+          <h5 class="choose-us-item__heading">${data.title}</h5>
+          <article class="choose-us-item__desc">
+            ${data.content}
+          </article>
+        </div>
+      </div>
+    `;
+    whyChooseUsList.appendChild(whyChooseUsEle);
+  });
+}
+
+if (pageSales) {
+  salesAndServiceData.sales.forEach((data) => {
+    const salesPage = document.createElement("section");
+    salesPage.classList.add("page-top-offset");
+    salesPage.id = data.navigatorId;
+    salesPage.innerHTML = `
+    <div class="container">
+    <div class="services-sales">
+      <div class="services-sales__short">
+        <div class="services-sales__title-desc">
+          <h2>${data.product}</h2>
+          <p>
+            ${data.productDesc}
+          </p>
+        </div>
+        <div class="services-sales__img">
+          <img
+            src="${data.orgImg}"
+            alt=""
+            width="100%"
+            height="100%"
+          />
+        </div>
+      </div>
+      <div class="services-sales__detail">
+        <p>
+         ${data.productLonDesc}
+        </p>
+      </div>
+    </div>
+  </div>
+    `;
+    pageSales.appendChild(salesPage);
+  });
+}
+
+if (pageServices) {
+  salesAndServiceData.services.forEach((data) => {
+    const servicesPage = document.createElement("section");
+    servicesPage.classList.add("page-top-offset");
+    servicesPage.id = data.navigatorId;
+    servicesPage.innerHTML = `
+    <div class="container">
+    <div class="services-sales">
+      <div class="services-sales__short">
+        <div class="services-sales__title-desc">
+          <h2>${data.service}</h2>
+          <p>
+            ${data.serviceDesc}
+          </p>
+        </div>
+        <div class="services-sales__img">
+          <img
+            src="${data.orgImg}"
+            alt=""
+            width="100%"
+            height="100%"
+          />
+        </div>
+      </div>
+      <div class="services-sales__detail">
+        <p>
+         ${data.serviceLonDesc}
+        </p>
+      </div>
+    </div>
+  </div>
+    `;
+    pageServices.appendChild(servicesPage);
+  });
+}
 // ****user actions**** //
 // on scroll set header color background
 window.addEventListener("scroll", handleScroll);
